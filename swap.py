aa7f157318136a33fc68456a45614b457b6d1827
Rohan Awate <awaterohan9535@gmail.com>
2022-02-25 18:20:50 +0530

    initial commit

---
+++ swap.py
@@ -0,0 +1,12 @@
+num1 = int( input("Please enter first number which you want to swap: "))  
+num2 = int( input("Please enter Second number which you want to swap: "))  
+
+print('\nFirst number before swapping: ',num1)
+print('Second number before swapping: ',num2)
+
+num3 = num1  
+num1 = num2  
+num2 = num3 
+
+print ("\n\nFirst number after swapping: ", num1)  
+print ("Second number after swapping: ", num2)  
